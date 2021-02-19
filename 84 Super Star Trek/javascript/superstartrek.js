050e268f07acf426df92d3bd1408cd04578cae5f
Les Orchard <me@lmorchard.com>
2021-02-19 15:06:55 -0800

    sst progress on short range scan display

--- 84 Super Star Trek/javascript/superstartrek.js
+++ 84 Super Star Trek/javascript/superstartrek.js
@@ -23,25 +23,31 @@
  */
 
 function print(...messages) {
-  console.log(messages.join(""));
+  console.log(messages.join(''));
 }
 
 const RND = () => Math.random();
 
-let G2$ = "";
-let Q$ = "";
+let G2$ = '';
+let Q$ = '';
+let C$ = '';
+let Z1 = 0;
+let Z2 = 0;
+let Z3 = 0;
+let R1 = 0;
+let R2 = 0;
 
-print("\n".repeat(10));
-print("                                    ,------*------,");
+print('\n'.repeat(10));
+print('                                    ,------*------,');
 print("                    ,-------------   '---  ------'");
 print("                     '-------- --'      / /");
 print("                         ,---' '-------/ /--,");
 print("                          '----------------'");
-print("");
-print("                    THE USS ENTERPRISE --- NCC-1701");
-print("\n".repeat(4));
+print('');
+print('                    THE USS ENTERPRISE --- NCC-1701');
+print('\n'.repeat(4));
 
-const Z$ = "                         ";
+const Z$ = '                         ';
 
 // 330 DIM N(3),D(8)
 
@@ -57,8 +63,8 @@ let S9 = 200;
 let S = 0;
 let B9 = 2;
 let K9 = 0;
-let X$ = "";
-let X0$ = " IS ";
+let X$ = '';
+let X0$ = ' IS ';
 
 const FND = (D) => sqrt((K(I, 1) - S1) ^ (2 + (K(I, 2) - S2)) ^ 2);
 const FNR = () => Math.floor(Math.random() * 7.98 + 1.01);
@@ -94,7 +100,7 @@ for (let i = 1; i <= 8; i++) {
   D[i] = 0;
 }
 
-const A1$ = "NAVSRSLRSPHATORSHEDAMCOMXXX";
+const A1$ = 'NAVSRSLRSPHATORSHEDAMCOMXXX';
 
 // 810 REM SETUP WHAT EXHISTS IN GALAXY . . .
 // 815 REM K3= # KLINGONS  B3= # STARBASES  S3 = # STARS
@@ -151,22 +157,22 @@ if (B9 === 0) {
 
 let K7 = K9;
 if (B9 !== 1) {
-  X$ = "S";
-  X0$ = " ARE ";
+  X$ = 'S';
+  X0$ = ' ARE ';
 }
 
-print("YOUR ORDERS ARE AS FOLLOWS:");
-print("     DESTROY THE ", K9, " KLINGON WARSHIPS WHICH HAVE INVADED");
-print("   THE GALAXY BEFORE THEY CAN ATTACK FEDERATION HEADQUARTERS");
+print('YOUR ORDERS ARE AS FOLLOWS:');
+print('     DESTROY THE ', K9, ' KLINGON WARSHIPS WHICH HAVE INVADED');
+print('   THE GALAXY BEFORE THEY CAN ATTACK FEDERATION HEADQUARTERS');
 print(
-  "   ON STARDATE ",
+  '   ON STARDATE ',
   T0 + T9,
-  "  THIS GIVES YOU ",
+  '  THIS GIVES YOU ',
   T9,
-  " DAYS.  THERE",
+  ' DAYS.  THERE',
   X0$
 );
-print("  ", B9, " STARBASE", X$, " IN THE GALAXY FOR RESUPPLYING YOUR SHIP");
+print('  ', B9, ' STARBASE', X$, ' IN THE GALAXY FOR RESUPPLYING YOUR SHIP');
 print();
 // REM PRINT"HIT ANY KEY EXCEPT RETURN WHEN READY TO ACCEPT COMMAND"
 I = RND(1);
@@ -193,10 +199,10 @@ if (Q1 >= 1 && Q1 <= 8 && Q2 >= 1 && Q2 <= 8) {
   buildQuadrantName();
   print();
   if (T0 == T) {
-    print("YOUR MISSION BEGINS WITH YOUR STARSHIP LOCATED");
+    print('YOUR MISSION BEGINS WITH YOUR STARSHIP LOCATED');
     print("IN THE GALACTIC QUADRANT, '", G2$, "'.");
   } else {
-    print("NOW ENTERING ", G2$, " QUADRANT . . .");
+    print('NOW ENTERING ', G2$, ' QUADRANT . . .');
   }
   print();
   K3 = Math.floor(G[Q1][Q2] * 0.01);
@@ -204,9 +210,9 @@ if (Q1 >= 1 && Q1 <= 8 && Q2 >= 1 && Q2 <= 8) {
   S3 = G[Q1][Q2] - 100 * K3 - 10 * B3;
 
   if (K3 != 0) {
-    print("COMBAT AREA      CONDITION RED", K3);
+    print('COMBAT AREA      CONDITION RED');
     if (S <= 200) {
-      print("   SHIELDS DANGEROUSLY LOW");
+      print('   SHIELDS DANGEROUSLY LOW');
     }
   }
 
@@ -224,52 +230,166 @@ Q$ = Z$ + Z$ + Z$ + Z$ + Z$ + Z$ + Z$ + Z$.substr(0, 17);
 
 // 1660 REM POSITION ENTERPRISE IN QUADRANT, THEN PLACE "K3" KLINGONS, &
 // 1670 REM "B3" STARBASES, & "S3" STARS ELSEWHERE.
-A$ = "<*>";
+A$ = '<*>';
 Z1 = S1;
 Z2 = S2;
 insertInStringForQuadrant();
 
 if (K3 >= 1) {
-  for (let i = 1; i <= K3; i++) {}
-  // 1720 FORI=1TOK3:GOSUB8590:A$="+K+":Z1=R1:Z2=R2
-// 1780 GOSUB8670:K(I,1)=R1:K(I,2)=R2:K(I,3)=S9*(0.5+RND(1)):NEXTI
+  // 1720
+  for (let i = 1; i <= K3; i++) {
+    findEmptyPlaceInQuadrant();
+    A$ = '+K+';
+    Z1 = R1;
+    Z2 = R2;
+    insertInStringForQuadrant();
+    K[i][1] = R1;
+    K[i][2] = R2;
+    K[i][3] = S9 * (0.5 + RND(1));
+  }
 }
-// 1820 IFB3<1THEN1910
-// 1880 GOSUB8590:A$=">!<":Z1=R1:B4=R1:Z2=R2:B5=R2:GOSUB8670
-// 1910 FORI=1TOS3:GOSUB8590:A$=" * ":Z1=R1:Z2=R2:GOSUB8670:NEXTI
 
+if (B3 >= 1) {
+  // 1820
+  findEmptyPlaceInQuadrant();
+  A$ = '>!<';
+  Z1 = R1;
+  B4 = R1;
+  Z2 = R2;
+  B5 = R2;
+  insertInStringForQuadrant();
+}
+
+for (let i = 1; i <= 5; i++) {
+  findEmptyPlaceInQuadrant();
+  A$ = ' * ';
+  Z1 = R1;
+  Z2 = R2;
+  insertInStringForQuadrant();
+}
 // 1980 GOSUB6430
+shortRangeSensorScanAndStartup();
+
+function shortRangeSensorScanAndStartup() {
+  // 6420 REM SHORT RANGE SENSOR SCAN & STARTUP SUBROUTINE
+  D0 = 0;
+  for (let i = S1 - 1; i <= S1 + 1; i++) {
+    for (let j = S1 - 1; j <= S2 + 1; j++) {
+      if (
+        Math.floor(i + 0.5) < 1 ||
+        Math.floor(i + 0.5) > 8 ||
+        Math.floor(j + 0.5) < 1 ||
+        Math.floor(j + 0.5) > 8
+      ) {
+        continue;
+      }
+
+      A$ = '>!<';
+      Z1 = i;
+      Z2 = j;
+      stringComparisonInQuadrantArray();
+
+      if (Z3 == 1) {
+        D0 = 1;
+        break;
+      }
+    }
+  }
+
+  if (D0 == 1) {
+    C$ = 'DOCKED';
+    E = E0;
+    P = P0;
+    print('SHIELDS DROPPED FOR DOCKING PURPOSES');
+    S = 0;
+  } else {
+    // 6650 IFK3>0THENC$="*RED*":GOTO6720
+    if (K3 > 0) {
+      C$ = 'RED';
+    }
+    // 6660 C$="GREEN":IFE<E0*.1THENC$="YELLOW"
+    C$ = 'GREEN';
+    if (E < E0 * 0.1) {
+      C$ = 'YELLOW';
+    }
+  }
+
+  if (D[2] < 0) {
+    // 6730
+    print();
+    print('*** SHORT RANGE SENSORS ARE OUT ***');
+    print();
+    return;
+  }
+
+  // 6770
+  const O1$ = '---------------------------------';
+  print(O1$);
+  let out = '';
+  for (let I = 1; I <= 8; I++) {
+    print(
+      ' ' +
+      Q$.substring((I - 1) * 24, (I - 1) * 24 + 24) +
+      [
+        '',
+        `        STARDATE           ${Math.floor(T*10)*.1}`,
+        `        CONDITION          ${C$}`,
+        `        QUADRANT           ${Q1} , ${Q2}`,
+        `        SECTOR             ${S1} , ${S2}`,
+        `        PHOTON TORPEDOES   ${Math.floor(P)}`,
+        `        TOTAL ENERGY       ${Math.floor(E+S)}`,
+        `        SHIELDS            ${Math.floor(S)}`,
+        `        KLINGONS REMAINING ${Math.floor(K9)}`,
+      ][I]
+    );
+  }
+  print(O1$);
+}
+
+// 8580 REM FIND EMPTY PLACE IN QUADRANT (FOR THINGS)
+function findEmptyPlaceInQuadrant() {
+  Z3 = 0;
+  while (Z3 == 0) {
+    R1 = FNR(1);
+    R2 = FNR(1);
+    A$ = '   ';
+    Z1 = R1;
+    Z2 = R2;
+    stringComparisonInQuadrantArray();
+  }
+}
+
+// 8820 REM STRING COMPARISON IN QUADRANT ARRAY
+function stringComparisonInQuadrantArray() {
+  Z1 = Math.floor(Z1 + 0.5);
+  Z2 = Math.floor(Z2 + 0.5);
+  const S8 = (Z2 - 1) * 3 + (Z1 - 1) * 24;
+  Z3 = 0;
+  if (Q$.substring(S8, S8 + 3) != A$) return;
+  Z3 = 1;
+}
 
 // 8660 REM INSERT IN STRING ARRAY FOR QUADRANT
 function insertInStringForQuadrant() {
-  const S8 = Math.floor(Z2 - 0.5) * 3 + Math.floor(Z1 - 0.5) * 24 + 1;
+  const S8 = Math.floor(Z2 - 0.5) * 3 + Math.floor(Z1 - 0.5) * 24;
   if (A$.length != 3) {
-    throw "ERROR";
+    throw 'ERROR';
   }
-  // 8680 IFS8=1THENQ$=A$+RIGHT$(Q$,189):RETURN
-  // 8690 IFS8=190THENQ$=LEFT$(Q$,189)+A$:RETURN
-  // 8700 Q$=LEFT$(Q$,S8-1)+A$+RIGHT$(Q$,190-S8):RETURN
-  if (S8 == 1) {
-    Q$ = A$ + Q$.substring(3, 192);
-  } else if (S8 == 190) {
-    Q$ = Q$.substring(0, 190) + A$;
-  } else {
-    Q$ = Q$.substring(0, S8 - 1) + A$ + Q$.substring(S8 + 1, 192);
-  }
+  Q$ = Q$.slice(0, S8) + A$ + Q$.slice(S8 + 3);
 }
 
 // 8780 REM PRINTS DEVICE NAME
 function printDeviceName() {
   G2$ = [
-    "", // FIXME: 1-based index
-    "WARP ENGINES",
-    "SHORT RANGE SENSORS",
-    "LONG RANGE SENSORS",
-    "PHASER CONTROL",
-    "PHOTON TUBES",
-    "DAMAGE CONTROL",
-    "SHIELD CONTROL",
-    "LIBRARY-COMPUTER",
+    '', // FIXME: 1-based index
+    'WARP ENGINES',
+    'SHORT RANGE SENSORS',
+    'LONG RANGE SENSORS',
+    'PHASER CONTROL',
+    'PHOTON TUBES',
+    'DAMAGE CONTROL',
+    'SHIELD CONTROL',
+    'LIBRARY-COMPUTER',
   ][R1];
 }
 
@@ -277,30 +397,30 @@ function printDeviceName() {
 function buildQuadrantName() {
   if (Z5 <= 4) {
     G2$ = [
-      "", // FIXME: 1-based index
-      "ANTARES",
-      "RIGEL",
-      "PROCYON",
-      "VEGA",
-      "CANOPUS",
-      "ALTAIR",
-      "SAGITTARIUS",
-      "POLLUX",
+      '', // FIXME: 1-based index
+      'ANTARES',
+      'RIGEL',
+      'PROCYON',
+      'VEGA',
+      'CANOPUS',
+      'ALTAIR',
+      'SAGITTARIUS',
+      'POLLUX',
     ][Z4];
   } else {
     G2$ = [
-      "", // FIXME: 1-based index
-      "SIRIUS",
-      "DENEB",
-      "CAPELLA",
-      "BETELGEUSE",
-      "ALDEBARAN",
-      "REGULUS",
-      "ARCTURUS",
-      "SPICA",
+      '', // FIXME: 1-based index
+      'SIRIUS',
+      'DENEB',
+      'CAPELLA',
+      'BETELGEUSE',
+      'ALDEBARAN',
+      'REGULUS',
+      'ARCTURUS',
+      'SPICA',
     ][Z4];
   }
   if (G5 != 1) {
-    G2$ = G2$ + ["", " I", " II", " III", " IV"][Z5 % 4];
+    G2$ = G2$ + ['', ' I', ' II', ' III', ' IV'][Z5 % 4];
   }
 }
